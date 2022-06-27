ccc3db42f753ecc2bf73d3ff72ee65be449e1d3e
pankowskit <dagi@10g.pl>
2022-06-27 11:26:11 +0200

    layout

--- app/app.js
+++ app/app.js
@@ -1,1 +1,26 @@
-angular.module("app",["ngRoute"]);
+var appCfg ={
+    navRoutes:[        
+        {navItem:true, path:"/",name:"Home",view:"app/views/index.html"},
+        {navItem:true, path:"/movies",name:"Movies",view:"app/views/movies.html"},
+        {navItem:true, path:"/plan",name:"Plan",view:"app/views/plan.html"},
+        {navItem:true, path:"/playlist",name:"Playlist",view:"app/views/playlist.html"},
+        {navItem:false, path:"/comingsoon",name:"Comingsoon",view:"app/views/comingsoon.html"},
+        {navItem:true, path:"/contact",name:"Contact",view:"app/views/contact.html"},
+        {navItem:true, path:"/login",name:"<span class='fa fa-user'></span>",view:"app/views/login.html",backLink:"/register"},
+        {navItem:false, path:"/register",name:"Register",view:"app/views/register.html",backLink:"/login"},
+        {navItem:false, path:"/privacy",name:"Privacy",view:"app/views/privacy.html"}  
+    ],
+    errRoute: {view:"app/views/error.html"}
+};
+
+angular.module("app",["ngRoute","ngSanitize"]);
+angular.module("app")
+.config(function($routeProvider) {
+    for(var i=0;i<appCfg.navRoutes.length;i++){
+        $routeProvider.when(appCfg.navRoutes[i].path,{templateUrl:appCfg.navRoutes[i].view});        
+    }
+    $routeProvider.otherwise({templateUrl:appCfg.errRoute.view});
+});
+angular.module("app").run(function($rootScope){
+    $rootScope.navMenu = appCfg.navRoutes;
+});
